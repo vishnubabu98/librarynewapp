f6da1914886676082262fb61ae2841d8d2b65c86
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-08 20:00:54 +0530

    add login pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
