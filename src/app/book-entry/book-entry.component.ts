23f2788c607437733b3264761739a604bcb2fcda
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-08 20:21:33 +0530

    add book entry page

---
+++ src/app/book-entry/book-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-entry',
+  templateUrl: './book-entry.component.html',
+  styleUrls: ['./book-entry.component.css']
+})
+export class BookEntryComponent {
+
+}
